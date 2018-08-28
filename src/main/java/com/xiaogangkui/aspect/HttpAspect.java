3716a7271a93292d4dd9bc8817e76fd03d666bbb
luchunyu <438001434@qq.com>
2018-08-28 10:57:03 +0800

    aop的添加

--- src/main/java/com/xiaogangkui/aspect/HttpAspect.java
+++ src/main/java/com/xiaogangkui/aspect/HttpAspect.java
@@ -1,6 +1,10 @@
 package com.xiaogangkui.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,21 +14,21 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class HttpAspect {
 
-//    @Pointcut("execution(* com.gaogao.platform.util.common.HttpUtil.*(..))")
-//    public void perform() {
-//
-//    }
-//
-//    @Around("perform()")
-//    public Object aroundPerform(ProceedingJoinPoint joinPoint) {
-//        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-//        System.out.println("方法名：" + signature.getMethod().getName());
-//        try {
-//            return joinPoint.proceed();
-//        } catch (Throwable throwable) {
-//            throwable.printStackTrace();
-//        }
-//        return null;
-//
-//    }
+    @Pointcut("execution(* com.xiaogangkui.util.common.HttpUtil.*(..))")
+    public void perform() {
+
+    }
+
+    @Around("perform()")
+    public Object aroundPerform(ProceedingJoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("方法名：" + signature.getMethod().getName());
+        try {
+            return joinPoint.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return null;
+
+    }
 }
