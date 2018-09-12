da546dcde748806166e6dcbdc8d417977a592be8
luchunyu <438001434@qq.com>
2018-09-12 16:08:42 +0800

    add

--- src/test/java/com/xiaogangkui/test/service/OkHttpTest.java
+++ src/test/java/com/xiaogangkui/test/service/OkHttpTest.java
@@ -3,7 +3,6 @@ package com.xiaogangkui.test.service;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.xiaogangkui.util.common.HttpUtil;
-import org.apache.commons.codec.binary.Base64;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import sun.misc.BASE64Encoder;
@@ -18,47 +17,28 @@ import java.util.Map;
 /**
  * @author Created by luchunyu
  */
-public class OkHttpTest extends BaseTest{
+public class OkHttpTest extends BaseTest {
 
     @Autowired
     private HttpUtil httpUtil;
-
-    @Test
-    public void testImage(){
-        String result = httpUtil.get("https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=R4B4v7ih5sCKtDQbGIEF4lHw&client_secret=2hEAfsI0IIX27oyxau6N4ToWdSDd3BXD");
-        Map<String, String> map = JSON.parseObject(result, new TypeReference<Map<String, String>>() {
-        });
-        System.out.println(map);
-
-    }
-
-    @Test
-    public void testScanImage() throws IOException {
-        String url = "https://aip.baidubce.com/rest/2.0/ocr/v1/vat_invoice?access_token=24.aa79fde9d279ada3457adda041235da6.2592000.1538135435.282335-11744150";
-        Map<String,String> map = new HashMap<>();
-        File file = new File("/Users/luchunyu/Desktop/WechatIMG173.png");
-        map.put("image",URLEncoder.encode(encodeImgageToBase64(file),"utf-8"));
-        Map<String,String> header = new HashMap<>();
-        header.put("Content-Type","application/x-www-form-urlencoded");
-        String result = httpUtil.postJsonByHeader(url, map, header);
-        System.out.println(result);
-
-    }
 
     public static String request(String httpUrl, String httpArg) {
         BufferedReader reader = null;
         String result = null;
         StringBuffer sbf = new StringBuffer();
-
         try {
+            // 用java JDK自带的URL去请求
             URL url = new URL(httpUrl);
-            HttpURLConnection connection = (HttpURLConnection) url
-                    .openConnection();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            // 设置该请求的消息头
+            // 设置HTTP方法：POST
             connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type",
-                    "application/x-www-form-urlencoded");
+            // 设置其Header的Content-Type参数为application/x-www-form-urlencoded
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             // 填入apikey到HTTP header
-            connection.setRequestProperty("apikey", "您自己的apikey");
+//            connection.setRequestProperty("apikey", "uml8HFzu2hFd8iEG2LkQGMxm");
+            // 将第二步获取到的token填入到HTTP header
+            connection.setRequestProperty("access_token", "24.d1efb53ac654bc39c66086a56494a4f4.2592000.1539246175.282335-11744515");
             connection.setDoOutput(true);
             connection.getOutputStream().write(httpArg.getBytes("UTF-8"));
             connection.connect();
@@ -77,7 +57,45 @@ public class OkHttpTest extends BaseTest{
         return result;
     }
 
-    public static String encodeImgageToBase64(File imageFile) {// 将图片文件转化为字节数组字符串，并对其进行Base64编码处理
+    @Test
+    public void testImage() {
+        String result = httpUtil.get("https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=83xP82Vmh3wTgGv6wtRKd2HL&client_secret=KV7HctuyM7j0IXm2RGW6yccfkLRfdRSE");
+        Map<String, String> map = JSON.parseObject(result, new TypeReference<Map<String, String>>() {
+        });
+        System.out.println(map);
+
+    }
+
+    @Test
+    public void testScanImage() throws IOException {
+        String url = "https://aip.baidubce.com/rest/2.0/ocr/v1/vat_invoice?access_token=24.d1efb53ac654bc39c66086a56494a4f4.2592000.1539246175.282335-11744515";
+        Map<String, String> map = new HashMap<>();
+        File file = new File("/Users/luchunyu/Desktop/WechatIMG173.png");
+        map.put("image", URLEncoder.encode(encodeImgageToBase64(file), "utf-8"));
+        Map<String, String> header = new HashMap<>();
+        header.put("Content-Type", "application/x-www-form-urlencoded");
+        String result = httpUtil.postJsonByHeader(url, map, header);
+        System.out.println(result);
+
+    }
+
+    @Test
+    public void testScan() {
+        // 获取本地的绝对路径图片
+        File file = new File("/Users/luchunyu/Desktop/WechatIMG173.jpg");
+        // 进行BASE64位编码
+        String imageBase = encodeImgageToBase64(file);
+        imageBase = imageBase.replaceAll("\r\n", "");
+        imageBase = imageBase.replaceAll("\\+", "%2B");
+        // 百度云的文字识别接口,后面参数为获取到的token
+        String httpUrl = "https://aip.baidubce.com/rest/2.0/ocr/v1/vat_invoice?access_token=24.d1efb53ac654bc39c66086a56494a4f4.2592000.1539246175.282335-11744515";
+        String httpArg = "image=" + imageBase;
+        String jsonResult = request(httpUrl, httpArg);
+        System.out.println("返回的结果--------->" + jsonResult);
+    }
+
+    public String encodeImgageToBase64(File imageFile) {
+        // 将图片文件转化为字节数组字符串，并对其进行Base64编码处理
         // 其进行Base64编码处理
         byte[] data = null;
         // 读取图片字节数组
@@ -86,12 +104,13 @@ public class OkHttpTest extends BaseTest{
             data = new byte[in.available()];
             in.read(data);
             in.close();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
         // 对字节数组Base64编码
         BASE64Encoder encoder = new BASE64Encoder();
         return encoder.encode(data);// 返回Base64编码过的字节数组字符串
     }
+
+
 }
