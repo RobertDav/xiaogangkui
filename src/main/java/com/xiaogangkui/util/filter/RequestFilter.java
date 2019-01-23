dbe0d76d2bedc252c17eff0ead2e0f978e0460a5
luchunyu <438001434@qq.com>
2019-01-23 20:44:28 +0800

    add

---
+++ src/main/java/com/xiaogangkui/util/filter/RequestFilter.java
@@ -0,0 +1,22 @@
+package com.xiaogangkui.util.filter;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @author Created by luchunyu
+ */
+@WebFilter(filterName = "requestFilter", urlPatterns = "/*")
+public class RequestFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // 防止流读取一次后就没有了, 所以需要将流继续写出去
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        ServletRequest requestWrapper = new BodyReaderHttpServletRequestWrapper(httpServletRequest);
+        filterChain.doFilter(requestWrapper, servletResponse);
+
+    }
+}
