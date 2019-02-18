64ca114b796da37c6dcc920c5a6a4b4373de494a
tuoxie <tuoxie@xiaogangkui.com>
2019-02-18 18:27:24 +0800

    add

--- src/test/java/com/xiaogangkui/test/service/CompanyServiceTest.java
+++ src/test/java/com/xiaogangkui/test/service/CompanyServiceTest.java
@@ -4,6 +4,7 @@ import com.xiaogangkui.dto.CompanyDto;
 import com.xiaogangkui.service.CompanyService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import java.util.List;
 
@@ -15,9 +16,16 @@ public class CompanyServiceTest extends BaseTest {
     @Autowired
     private CompanyService companyService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
     @Test
     public void testQueryAll(){
         List<CompanyDto> companyDtos = companyService.queryAll();
         System.out.println(companyDtos);
+    }
+    @Test
+    public void testRedis(){
+        Object testKey = redisTemplate.opsForValue().get("testKey");
+        System.out.println(testKey);
     }
 }
