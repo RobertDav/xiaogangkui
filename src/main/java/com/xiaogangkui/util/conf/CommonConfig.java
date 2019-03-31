3f740e1b5255bb6379eed5f3fa2485401d7520c3
luchunyu <438001434@qq.com>
2019-03-31 17:06:27 +0800

    add

--- src/main/java/com/xiaogangkui/util/conf/CommonConfig.java
+++ src/main/java/com/xiaogangkui/util/conf/CommonConfig.java
@@ -1,6 +1,9 @@
 package com.xiaogangkui.util.conf;
 
 
+import com.suanhu.image.service.dto.AuthTokenService;
+import com.suanhu.image.service.impl.ImageBaseService;
+import com.suanhu.image.service.impl.QiniuService;
 import com.xiaogangkui.util.common.BeanMapperUtil;
 import com.xiaogangkui.util.common.BeanUtil;
 import com.xiaogangkui.util.common.ExcelUtil;
@@ -38,5 +41,22 @@ public class CommonConfig {
         return new BeanUtil();
     }
 
+    @Bean
+    public QiniuService getQiniuService(){
+        return new QiniuService();
+    }
 
+    @Bean
+    public ImageBaseService getImageBaseService(){
+        ImageBaseService imageBaseService = new ImageBaseService();
+        imageBaseService.setAccessKey("GeBgwnV9CB6QfEtc9eV98Ern986TQHwQSf-cJQE6");
+        imageBaseService.setSecretKey("ebxR56ltoLlptHDIW4JwNKcaRSiaDijuzDvQtSov");
+        imageBaseService.setPublicDomain("o9w8tdosw.bkt.clouddn.com");
+        imageBaseService.setPrivateDomain("o9x64wd7c.bkt.clouddn.com");
+        return imageBaseService;
+    }
+    @Bean
+    public AuthTokenService authTokenService(){
+        return new AuthTokenService();
+    }
 }
