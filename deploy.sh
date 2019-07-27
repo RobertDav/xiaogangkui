7ffd8387cbbd52335dc30e9b60ce01ac4ad2eb84
luchunyu <438001434@qq.com>
2019-07-27 15:20:31 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ mvn clean install -DskipTests
 scp -P 58422 target/cantaloupe-1.0-SNAPSHOT.jar root@obas:/usr/local/tomcat/
 
 
-# 在obas 机器上的/usr/local/tomcat 先kill 掉当前项目的进程 然后执行 start.sh
+# 在obas 机器上的/usr/local/tomcat 先kill 掉当前项目的进程 然后执行 ./start.sh
